59a94e627fcf12f05d9985b99365be94892fab4d
Fabian Tewes <fabian.tewes@gmail.com>
2020-12-01 11:28:39 +0100

    added search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+  def index
+    if params[:query].present?
+      @categories = Category.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @categories = Category.all
+    end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ class PostsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
