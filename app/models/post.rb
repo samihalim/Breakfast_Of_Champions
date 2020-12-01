338ae807b60293d29985d5acc97e109dc722cc15
Sami Halim <samihalim@gmail.com>
2020-12-01 16:38:14 +0100

    destroy actions working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :feedbacks
-  has_many :post_categories
+  has_many :feedbacks, dependent: :destroy
+  has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
 
   has_many_attached :photos
