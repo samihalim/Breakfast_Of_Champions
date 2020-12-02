c985e3a4c47552bf21024f44593ac20ddf7dd677
Imke van Rossum <imkevanrossum@hotmail.com>
2020-12-02 17:29:40 +0100

    Made edit and delete button on post/show only visible when creator of post is logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
 
