95bbc280267cd9764a6e2cbc4183a8a56629c719
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-12-02 11:16:47 +0100

    added biography, location, experience to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_155933) do
+ActiveRecord::Schema.define(version: 2020_12_02_101537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(version: 2020_12_01_155933) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_name"
+    t.string "biography"
+    t.string "location"
+    t.string "experience"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
