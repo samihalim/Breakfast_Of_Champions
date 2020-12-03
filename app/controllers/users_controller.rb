522329809324a8ddb7b510eae83d5ff270d3940e
hanna sonnenberg <sonnenberghanna@googlemail.com>
2020-12-03 12:18:39 +0100

    add edit und update action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,35 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username((params[:id]))
     @feedbacks = @user.feedbacks
+  end
 
+  def edit
+    @user = User.find_by_username((params[:id]))
+  end
+
+  def update
+    user = User.find_by_username((params[:id]))
+    if user.update(user_params)
+      redirect_to user_profile_path(current_user.username), notice: 'Your Profile was successfully updated.'
+    else
+      render :edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :rating, :address)
   end
 end
 
+
+
+
+
+
+  private
+
+
+  def feedback_params
+    params.require(:feedback).permit(:post_id, :description, :feedback_id,)
+  end
