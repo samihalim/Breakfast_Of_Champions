d292ba9481415912a7f7310b5df9e584e9cfedaf
Imke van Rossum <imkevanrossum@hotmail.com>
2020-12-04 14:47:47 +0100

    add ability to update profile picture

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :url, :experience, :biography, :location])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :url, :experience, :biography, :location, :photo])
   end
 
   def after_update_path_for(resource)
