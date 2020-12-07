c5d231bd20e50ab1005ba81b17a3db386d4ca32d
Sami Halim <samihalim@gmail.com>
2020-12-07 15:30:26 +0100

    navbar

---
+++ app/javascript/channels/user_channel.js
@@ -0,0 +1,19 @@
+import consumer from "./consumer";
+
+const initChatroomCable = () => {
+  const flagContainer = document.getElementById('flag-container');
+  if (flagContainer) {
+    const id = flagContainer.dataset.chatroomId;
+    console.log('found')
+    consumer.subscriptions.create({ channel: "UserChannel", id: id },{
+      connected() {
+        console.log('connected');
+      },
+      received(data) {
+        console.log(data); // called when data is broadcast in the cable
+      },
+    });
+  }
+}
+
+export { initChatroomCable };
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/:id', to: 'users#show', as: :user_profile
+  resources :notifications, only: :show
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do
