8013417937e469a9ee84f475324e3a718cb322f2
Imke van Rossum <imkevanrossum@hotmail.com>
2020-12-07 17:25:06 +0100

    merged master

--- db/schema.rb
+++ db/schema.rb
@@ -141,6 +141,15 @@ ActiveRecord::Schema.define(version: 2020_12_07_141541) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "upvotes", force: :cascade do |t|
+    t.bigint "feedback_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["feedback_id"], name: "index_upvotes_on_feedback_id"
+    t.index ["user_id"], name: "index_upvotes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -167,4 +176,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_141541) do
   add_foreign_key "post_categories", "categories"
   add_foreign_key "post_categories", "posts"
   add_foreign_key "posts", "users"
+  add_foreign_key "upvotes", "feedbacks"
+  add_foreign_key "upvotes", "users"
 end
