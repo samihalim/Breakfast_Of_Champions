b266ceabf9baf9fb4c7f74feacbea51359e8a6a1
Sami Halim <samihalim@gmail.com>
2020-12-07 16:03:23 +0100

    linked flag to notifications page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/:id', to: 'users#show', as: :user_profile
-  resources :notifications, only: :show
-  root to: 'pages#home'
   resources :notifications, only: :index
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :feedbacks, only: [:create, :edit, :update, :show]
